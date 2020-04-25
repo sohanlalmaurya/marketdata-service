cbdc03ffd931b79400c1bfef22f9d07d5281d19d
sohan.maurya <sohanlal.maurya@gmail.com>
2020-04-25 22:31:02 +0530

    implement Ribbon feature

--- src/main/java/com/sapient/marketdata/service/feign/RefdataFeign.java
+++ src/main/java/com/sapient/marketdata/service/feign/RefdataFeign.java
@@ -2,13 +2,16 @@ package com.sapient.marketdata.service.feign;
 
 import java.util.List;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.sapient.marketdata.service.model.TradesModel;
 
-@FeignClient(name = "refdata-service", url = "localhost:8083")
+//@FeignClient(name = "refdata-service", url = "localhost:8083")
+@FeignClient(name = "refdata-service")
+@RibbonClient(name = "refdata-service")
 public interface RefdataFeign {
 	
 	@GetMapping("/trades")
